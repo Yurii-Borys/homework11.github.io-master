dcb10d2913770655e28ffa5e3bf651f2c41ca794
Yuriy <leolux.avto@gmail.com>
2019-12-11 15:28:12 +0200

    click but add

--- app.js
+++ app.js
@@ -19,6 +19,63 @@ function showSlides() {
   setTimeout(showSlides, 2000); 
 }
   
+/*!-- Buy content -->*/
+ 
+let modal = document.getElementById("myModal");
+
+let buttons = document.querySelectorAll('.btn-buy'),
+    index, button;
+
+for (index = 0; index < buttons.length; index++) {
+    button = buttons[index];
+    button.addEventListener('click', clickHandler);
+    
+}
+
+function clickHandler(event) {
+    modal.style.display = "block";
+    event.preventDefault();
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
-  
+
+
+
+
+/*!-- Buy content -->*/
+ 
+let modal2 = document.getElementById("myModalCart");
+
+let btn2 = document.querySelector("#cart");
+console.log(btn2);
+
+let span2 = document.getElementsByClassName("close-cart")[0];
+
+btn2.addEventListener ("click",function() {
+  modal2.style.display = "block";
+} );
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+
+
